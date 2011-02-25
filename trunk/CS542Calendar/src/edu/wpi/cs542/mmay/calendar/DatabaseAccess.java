710389d9803c29186f7e2da656b4ebd10f8bd40b
mike.molignano <mike.molignano@bd2ba464-9b5b-a973-ef2e-c8691a0f1cae>
2011-02-25 15:07:54 +0000

    DatabaseAccess class created

---
+++ trunk/CS542Calendar/src/edu/wpi/cs542/mmay/calendar/DatabaseAccess.java
@@ -0,0 +1,17 @@
+package edu.wpi.cs542.mmay.calendar;
+
+/**
+ * 
+ *
+ * @author Mike Molignano
+ * @author Andrew Yee
+ *
+ */
+public class DatabaseAccess {
+	
+	public static boolean addEvent() {
+		
+		return false;
+	}
+
+}

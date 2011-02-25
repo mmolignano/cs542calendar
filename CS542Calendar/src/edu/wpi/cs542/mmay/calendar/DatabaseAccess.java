06c6c77e867b94b97d908521f32f7587547688c1
mike.molignano <mike.molignano@bd2ba464-9b5b-a973-ef2e-c8691a0f1cae>
2011-02-25 17:09:30 +0000

    Added ShowCalendar Servlet

--- CS542Calendar/src/edu/wpi/cs542/mmay/calendar/DatabaseAccess.java
+++ CS542Calendar/src/edu/wpi/cs542/mmay/calendar/DatabaseAccess.java
@@ -1,10 +1,12 @@
 package edu.wpi.cs542.mmay.calendar;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.jdo.Extent;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Transaction;
-
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
 
 /**
  * 
@@ -26,22 +28,49 @@ public class DatabaseAccess {
 		Transaction tx = pm.currentTransaction();
 		
 		try {	
-			tx.begin();
+//			tx.begin();
 	
 			pm.makePersistent(ev);
 			//datastore.put(ev);
 	
-			tx.commit();
+//			tx.commit();
 		}finally	{
-			if (tx.isActive()){
-				tx.rollback();
-				returner = false;
-			}
+//			if (tx.isActive()){
+//				tx.rollback();
+//				returner = false;
+//			}
 		}
 		
 		pm.close();
 
 		return returner;
+	}
+	
+	public static ArrayList<Event> getEvents(){
+		ArrayList<Event> returner = new ArrayList<Event>();
+		
+		Collection<Event> events = fetchAllEvents();
+	
+		Iterator<Event> iterator = events.iterator();
+		while(iterator.hasNext()){
+			Event event = iterator.next();
+			returner.add(event);
+		}
+		
+		return returner;
+	}
+	
+	public static Collection<Event> fetchAllEvents() {
+		PersistenceManager pm = PMF.getInstance().getPersistenceManager();
+		ArrayList<Event> returner = new ArrayList<Event>();
+		Extent<Event> events = pm.getExtent(Event.class);
+
+		Iterator<Event> iterator = events.iterator();
+		while (iterator.hasNext()){
+			returner.add(iterator.next());
+		}
+		pm.close();
+		return (Collection<Event>) returner;
 	}
 
 }

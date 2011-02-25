c6d53e6b1f7c5e409f5fffc3c850383c06a4b859
mike.molignano <mike.molignano@bd2ba464-9b5b-a973-ef2e-c8691a0f1cae>
2011-02-25 17:09:30 +0000

    Added ShowCalendar Servlet

---
+++ trunk/CS542Calendar/src/edu/wpi/cs542/mmay/calendar/ShowCalendarServlet.java
@@ -0,0 +1,31 @@
+package edu.wpi.cs542.mmay.calendar;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class ShowCalendarServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		
+		//Event ev = new Event("event 1", new Date(2012, 1, 1, 0, 0), "world", "end of the world");
+		
+		// TODO Auto-generated method stub
+		
+		//DatabaseAccess access = new DatabaseAccess();
+		
+		ArrayList<Event> events = DatabaseAccess.getEvents();
+		resp.setContentType("text/plain");
+		
+		// Print Headers
+		resp.getWriter().println("NAME\n");
+		
+		for (Event e1 : events) {
+			resp.getWriter().println(e1.getEventName());
+		}
+	}
+}

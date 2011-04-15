f0fe958b5a3948ce7eaadf166e779df05c41c5d1
mike.molignano <mike.molignano@bd2ba464-9b5b-a973-ef2e-c8691a0f1cae>
2011-04-15 20:11:38 +0000

    Working on EventShareServlet.

--- trunk/CS542Calendar/src/edu/wpi/cs542/mmay/calendar/servlet/EventShareServlet.java
+++ trunk/CS542Calendar/src/edu/wpi/cs542/mmay/calendar/servlet/EventShareServlet.java
@@ -2,6 +2,9 @@ package edu.wpi.cs542.mmay.calendar.servlet;
 
 import javax.servlet.http.HttpServlet;
 
+// Working on this one now...
+// Mike
+
 @SuppressWarnings("serial")
 public class EventShareServlet extends HttpServlet {
 

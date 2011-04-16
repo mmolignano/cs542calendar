cd2963526ed710f34ca739e7773a99db1f89357f
mike.molignano <mike.molignano@bd2ba464-9b5b-a973-ef2e-c8691a0f1cae>
2011-04-16 23:03:36 +0000

    Testing user stuff

--- CS542Calendar/src/edu/wpi/cs542/mmay/calendar/servlet/DummyServlet.java
+++ CS542Calendar/src/edu/wpi/cs542/mmay/calendar/servlet/DummyServlet.java
@@ -2,7 +2,12 @@ package edu.wpi.cs542.mmay.calendar.servlet;
 
 import java.io.IOException;
 
+
 import javax.servlet.http.*;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 import edu.wpi.cs542.mmay.calendar.DatabaseAccess;
 import edu.wpi.cs542.mmay.calendar.kinds.Event;
@@ -12,13 +17,26 @@ public class DummyServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		
-		boolean success = DatabaseAccess.addEvent(new Event());
-		resp.setContentType("text/plain");
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
 		
-		if (success) {
-			resp.getWriter().println("Add was successful");
-		} else {
-			resp.getWriter().println("Add failed");
-		}
+		if (user != null) {
+            resp.setContentType("text/plain");
+            resp.getWriter().println("Hello, " + user.getNickname());
+        } else {
+            resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+        }
+		
+		
+		
+//		boolean success = DatabaseAccess.addEvent(new Event());
+//		resp.setContentType("text/plain");
+		resp.getWriter().println("Add was successful");
+		
+//		if (success) {
+//			resp.getWriter().println("Add was successful");
+//		} else {
+//			resp.getWriter().println("Add failed");
+//		}
 	}
 }

8c07bdefe914c1c3a00e44c7e78fa0538a80b50e
mike.molignano <mike.molignano@bd2ba464-9b5b-a973-ef2e-c8691a0f1cae>
2011-04-16 23:03:25 +0000

    Trying to get this to fucking work..asdfms;dfm,as/.,dfga,sdfkjkasljfdf

--- trunk/CS542Calendar/src/edu/wpi/cs542/mmay/calendar/servlet/CalendarAddServlet.java
+++ trunk/CS542Calendar/src/edu/wpi/cs542/mmay/calendar/servlet/CalendarAddServlet.java
@@ -6,8 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 import edu.wpi.cs542.mmay.calendar.DatabaseAccess;
 import edu.wpi.cs542.mmay.calendar.kinds.Calendar;
+import edu.wpi.cs542.mmay.calendar.PMF;
 
 public class CalendarAddServlet extends HttpServlet {
 
@@ -15,26 +20,33 @@ public class CalendarAddServlet extends HttpServlet {
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		
+		// Check that a user is logged in...
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		if (user == null) {
+			resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+        }
+		
 		//Event ev = new Event();
 		Calendar calendar = new Calendar();
 		
-		calendar.setName(req.getParameter("name"));
-		calendar.setOwner(req.getParameter("owner"));
+//		calendar.setName(req.getParameter("name"));
+//		calendar.setOwner(user.getNickname());
 		
 		
 		
-		//boolean success = DatabaseAccess.addEvent(ev); 
-		boolean success = DatabaseAccess.addCalendar(calendar);
-		
-		resp.setContentType("text/plain");
-		
-		if (success) {
-			resp.getWriter().println("Added Calendar:\n");
-			resp.getWriter().println("Calendar Name: " + calendar.getName());
-			resp.getWriter().println("Calendar Owner: " + calendar.getOwner());
-			
-		} else {
-			resp.getWriter().println("Add Unsuccessful");
-		}
+//		//boolean success = DatabaseAccess.addEvent(ev); 
+//		boolean success = DatabaseAccess.addCalendar(calendar);
+//		
+//		resp.setContentType("text/plain");
+//		
+//		if (success) {
+//			resp.getWriter().println("Added Calendar:\n");
+//			resp.getWriter().println("Calendar Name: " + calendar.getName());
+//			resp.getWriter().println("Calendar Owner: " + calendar.getOwner());
+//			
+//		} else {
+//			resp.getWriter().println("Add Unsuccessful");
+//		}
 	}
 }
